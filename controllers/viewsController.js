7bda14f5abe50cabdbd304a5fa7cef137e1bdab7
Chuck Yu <charles.yu4@gmail.com>
2021-03-22 21:41:30 -0400

    second commit

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,18 +1,12 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const colors = require('../public/js/colors');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 3) render template using tour data from (1)
   // this will look in the /views (set in app.js) folder for 'overview.pug' (pug engine also specified in app.js)
   res.status(200).render('index', {
     title: `Flip a coin`,
-  });
-});
-
-exports.getRed = catchAsync(async (req, res, next) => {
-  // 3) render template using tour data from (1)
-  // this will look in the /views (set in app.js) folder for 'overview.pug' (pug engine also specified in app.js)
-  res.status(200).render('red', {
-    title: `Flip a coin`,
+    colors,
   });
 });
