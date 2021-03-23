00cd13ac433229eadf2cbefdffed0b7ad72430e9
Chuck Yu <charles.yu4@gmail.com>
2021-03-23 16:19:53 -0400

    Fixed - working version of io-poc

--- public/js/colorFactory.js
+++ public/js/colorFactory.js
@@ -1,10 +1,10 @@
-import { Blue } from './colors/blue.js';
-import { Green } from './colors/green.js';
-import { Red } from './colors/red.js';
-import { colorList } from './colors.js';
+import Blue from './colors/blue.js';
+import Green from './colors/green.js';
+import Red from './colors/red.js';
+import colorList from './colors.js';
 class ColorFactory {
   constructor() {
-    this.colorList = require('./colors');
+    this.colorList = colorList;
   }
 
   getColor(color) {
